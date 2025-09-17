cde9ec56fd0b0203f976c8882c0d0efb37e49489
jovven <shadowguy8888@gmail.com>
2025-09-17 20:19:38 +0800

    Initial commit: basic Spring Boot app

---
+++ src/main/java/com/example/ncs_demo/NcsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncs_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ncs_demo/NcsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncs_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
